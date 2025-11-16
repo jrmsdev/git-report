9a95c3c335057dfa8f2185c6bc2dcf340f857f87
Jeremías <jrmsdev@gmail.com>
2025-11-16 18:29:39 -0300

    docker mount source dir as read only

--- docker/devel.sh
+++ docker/devel.sh
@@ -3,6 +3,6 @@ set -eu
 exec docker run -it --rm -u devel \
 	--name git-report \
 	--hostname git-report.local \
-	-v "${PWD}:/opt/src" \
+	-v "${PWD}:/opt/src:ro" \
 	--workdir /opt/src \
 	jrmsdev/git-report
