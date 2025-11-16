6c3bb74684ee122eac4f899ad845b62b0bb8203d
Jeremías <jrmsdev@gmail.com>
2025-11-16 19:26:18 -0300

    go mod init

--- docker/devel.sh
+++ docker/devel.sh
@@ -3,7 +3,7 @@ set -eu
 exec docker run -it --rm -u devel \
 	--name git-report \
 	--hostname git-report.local \
-	-v "${PWD}:/opt/src:ro" \
+	-v "${PWD}:/opt/src" \
 	--workdir /opt/src \
     -p 127.0.0.1:8001:8001 \
 	jrmsdev/git-report
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jrmsdev/git-report
+
+go 1.24.9
